1938442d93e7b183da162ba3b2b7c3238e627613
AxWZK <axwzk314@gmail.com>
2015-01-11 17:55:54 -0800

    first hack

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#--- Hacks performed by AxWZK 
+#--- January 11, 2015 
+#
+## Followed the example closely. All the pieces were there to meet the 
+## requirements specified in the assignment. No sense in reinventing
+## The wheel. I did change variable names to help avoid confusion
 
-## Hacks performed by AxWZK
-
-## Write a short comment describing this function
-
+#------
+## Follows the example in structure. Modified function names to improve
+## clarity and avoid confusion when executing code
+#------
 makeCacheMatrix <- function(x = matrix()) {
-
+	# initialize
+	inv <- NULL
+	
+	# define funcitons
+	setMatrix <- function(mat) { x <<- mat; inv <<- NULL}
+	getMatrix <- function() x
+	setInverse <- function(inverse) inv <<- inverse
+	getInverse <- function() inv
+	
+	# define 1x4 output "matrix"
+	list(setMat = setMatrix, 
+		getMat = getMatrix, 
+		setInv = setInverse, 
+		getInv = getInverse)
 }
-
-
-## Write a short comment describing this function
-
+#------
+## This implementation makes use of the 'solve()' function, 
+## which will return the inverse of a square matrix.
+#------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# initialize
+	inv <- x$getInv()
+	
+	# first check to see if inv already has a value (matrix) assigned
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	
+	# calculate the inverse if it is not in cache
+	data <- x$getMat()
+	inv <- solve(data)
+	x$setInv(inv)
+	inv
 }
